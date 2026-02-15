d691230eda9964cb5cb4f95efa72a6bfcaf8c992
Matthew Moukaddem <9834543+matt953@users.noreply.github.com>
2026-02-15 11:39:53 +1100

    Add edge264 decoder for H.264/MVC 3D Blu-ray support Integrates the edge264 decoder library for H.264/MVC decoding with side-by-side stereoscopic output support. This enables proper MVC 3D Blu-ray playback when combined with https://github.com/matt953/jellyfin

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -765,6 +765,7 @@ extern const FFCodec ff_libcodec2_encoder;
 extern const FFCodec ff_libcodec2_decoder;
 extern const FFCodec ff_libdav1d_decoder;
 extern const FFCodec ff_libdavs2_decoder;
+extern const FFCodec ff_libedge264_decoder;
 extern const FFCodec ff_libfdk_aac_encoder;
 extern const FFCodec ff_libfdk_aac_decoder;
 extern const FFCodec ff_libgsm_encoder;
